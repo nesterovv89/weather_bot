8c9e401da9e87d15a79cb711a0bbf94ad9c2b074
Павел Нестеров <necterov.p@gmail.com>
2024-10-15 23:08:08 +0300

    url correction

--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ url = c.URL
 
 async def get_weather(city):
     """Запрос погоды от АПИ по кастомному городу"""
-    response = requests.get(f'{url+city+'?format=4'}')
+    response = requests.get(f'{url}{city}?format=4')
     if response.status_code == 200:
         return response.text
     elif response.status_code == 404:
@@ -18,7 +18,7 @@ async def get_weather(city):
 
 async def get_weather_current_location():
     """Запрос погоды от АПИ по текущему месту"""
-    response = requests.get(f'{url+'?format=4'}')
+    response = requests.get(f'{url}?format=4')
     if response.status_code == 200:
         return response.text
     elif response.status_code == 404:
