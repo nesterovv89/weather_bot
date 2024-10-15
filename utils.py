9b6e8125026b6b7443266fa37f143ffe106223f8
Павел Нестеров <necterov.p@gmail.com>
2024-10-16 00:01:53 +0300

    completed

--- utils.py
+++ utils.py
@@ -21,7 +21,6 @@ async def get_weather_current_location():
     """Запрос погоды от АПИ по текущему месту"""
     geo = geocoder.ip('me')
     city = geo.city
-    print (city)
     response = requests.get(f'{url}{city}?format=4')
     if response.status_code == 200:
         return response.text
